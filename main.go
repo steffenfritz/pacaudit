dd01471e19a6468763930b29e062a4d0d2151e58
steffenfritz <steffenfritz@amp-off.com>
2017-05-07 00:13:00 +0200

    v.0.4.0-DEV

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"text/tabwriter"
+
+	"github.com/pkg/profile"
+	//	"text/tabwriter"
 )
 
 // source url
@@ -22,7 +26,7 @@ const url string = "https://security.archlinux.org/vulnerable/json"
 var nagios = flag.Bool("n", false, "run pacaudit as nagios plugin. If run in this mode it returns OK, WARNING or CRITICAL.")
 var verbose = flag.Bool("v", false, "run pacaudit in verbose mode. This prints the severity and all related CVE.")
 
-// issue struct.
+// issue struct
 type issue struct {
 	Name       string   `json:"name"`
 	Packages   []string `json:"packages"`
@@ -44,48 +48,50 @@ type output struct {
 
 // main function
 func main() {
+	defer profile.Start(profile.CPUProfile).Stop()
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 	flag.Parse()
-	compare(parse(fetchrecent()), readDBContent(readDBPath()))
+	compare(parse(fetchrecent()), readDBContent(readDBPath()), w)
 }
 
 // compare installed package list with vulnerable package list
-func compare(m []issue, locpkglist []string) {
-	pkg_listed := make(map[string]bool)
-	sev_warning := false
-	sev_crit := false
+func compare(m []issue, locpkglist []string, w *tabwriter.Writer) {
+	pkgListed := make(map[string]bool)
+	sevWarning := false
+	sevCrit := false
 
 	for _, entry := range m {
 		for _, ipkgname := range entry.Packages {
 			for _, lpkgname := range locpkglist {
 				if strings.HasPrefix(lpkgname, ipkgname+"-") {
-					pkg_listed[lpkgname] = true
+					pkgListed[lpkgname] = true
 					if *verbose {
-						fmt.Println("\n+ + +\n" + ipkgname + " is vulnerable.")
-						fmt.Println("Severity: " + entry.Severity)
-						fmt.Println("\nCVE:")
-						for _, cve := range entry.Issues {
-							fmt.Println(cve)
+						cveTemp := entry.Issues[0]
+						for _, cve := range entry.Issues[1:] {
+							cveTemp += "\n" + "\t" + "\t" + cve
 						}
+						fmt.Fprintln(w, lpkgname+"\t"+entry.Severity+"\t"+cveTemp)
 					} else if !*nagios {
 						fmt.Println(ipkgname + " is vulnerable.")
 					}
 
 					if *nagios {
 						if (entry.Severity == "Low") || (entry.Severity == "Medium") {
-							sev_warning = true
+							sevWarning = true
 						} else if (entry.Severity == "High") || (entry.Severity == "Critical") {
-							sev_crit = true
+							sevCrit = true
 						}
 					}
 				}
 			}
 		}
 	}
+	w.Flush()
 	if *nagios {
-		if sev_crit {
+		if sevCrit {
 			fmt.Println("Critical")
 			return
-		} else if sev_warning {
+		} else if sevWarning {
 			fmt.Println("Warning")
 			return
 		} else {
@@ -93,7 +99,7 @@ func compare(m []issue, locpkglist []string) {
 			return
 		}
 	}
-	fmt.Println("\n" + strconv.Itoa(len(pkg_listed)) + " vulnerable package(s) installed.")
+	fmt.Println("\n" + strconv.Itoa(len(pkgListed)) + " vulnerable package(s) installed.")
 }
 
 // a generic error check
@@ -128,7 +134,7 @@ func parse(body []byte) []issue {
 // get location of local pkg db
 func readDBPath() string {
 
-	var pkg_path string
+	var pkgPath string
 
 	f, err := os.Open("/etc/pacman.conf")
 
@@ -137,12 +143,12 @@ func readDBPath() string {
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		if strings.HasPrefix(scanner.Text(), "DBPath") {
-			pkg_path = string(scanner.Text())
+			pkgPath = string(scanner.Text())
 		} else {
-			pkg_path = "/var/lib/pacman/local"
+			pkgPath = "/var/lib/pacman/local"
 		}
 	}
-	return pkg_path
+	return pkgPath
 }
 
 // get local pkg list
